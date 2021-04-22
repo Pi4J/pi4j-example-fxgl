3412c2f85657378a487639f4f25ef4ad74878f57
Frank Delporte <frank@webtechie.be>
2021-04-22 20:36:48 +0200

    GTK run script

--- assets/run.sh
+++ assets/run.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-java  \
-  -Dmonocle.egl.lib=/opt/arm32fb-sdk/lib/libgluon_drm.so \
+java \
+  -Dglass.platform=gtk \
   -Djava.library.path=/opt/arm32fb-sdk/lib \
   -Dmonocle.platform.traceConfig=false \
   -Dprism.verbose=false \
   -Djavafx.verbose=false \
-  -Dmonocle.platform=EGL \
   --module-path .:/opt/arm32fb-sdk/lib \
   --add-modules javafx.controls \
   --module com.pi4j.example/com.pi4j.example.FxglExample $@
