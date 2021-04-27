69d93a9155bd4b642b6b67c3c8a8d555647ef3e8
Frank Delporte <frank@webtechie.be>
2021-04-27 13:16:37 +0200

    Correct run script

--- assets/run.sh
+++ assets/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 java \
   -Dglass.platform=gtk \
-  -Djava.library.path=/opt/arm32fb-sdk/lib \
+  -Djava.library.path=/opt/javafx-sdk-17/lib \
   -Dmonocle.platform.traceConfig=false \
   -Dprism.verbose=false \
   -Djavafx.verbose=false \
-  --module-path .:/opt/arm32fb-sdk/lib \
+  --module-path .:/opt/javafx-sdk-17/lib \
   --add-modules javafx.controls \
   --module com.pi4j.example/com.pi4j.example.FxglExample $@
