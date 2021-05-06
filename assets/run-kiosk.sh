2f4d0cf9a6298d99b57b6e32ed5e6cec23089d3b
Cyril <cymojo>
2021-05-06 16:51:57 +0200

    Added kiosk run script

---
+++ assets/run-kiosk.sh
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env bash
+/sbin/init 3
+export ENABLE_GLUON_COMMERCIAL_EXTENSIONS=true
+java \
+  -Degl.displayid=/dev/dri/card0 \
+  -Dmonocle.egl.lib=/opt/javafx-sdk-17/lib/libgluon_drm-1.1.3.so \
+  -Djava.library.path=/opt/javafx-sdk-17/lib \
+  -Dmonocle.platform.traceConfig=false \
+  -Dprism.verbose=false \
+  -Djavafx.verbose=false \
+  -Dmonocle.platform=EGL \
+  --module-path .:/opt/javafx-sdk-17/lib \
+  --add-modules javafx.controls \
+  --module com.pi4j.example/com.pi4j.example.FxglExample $@
+/sbin/init 5
